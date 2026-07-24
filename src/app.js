6961c60eebdbc61c1a2b6584900e0dc13b3c03d5
Sarthak Kasat <kasat4309@gmail.com>
2026-07-25 01:41:04 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+
 import adminRoutes from "./routes/adminRoutes.js";
 import propertyRoutes from "./routes/propertyRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
@@ -8,26 +9,27 @@ import testimonialRoutes from "./routes/testinomalRoute.js";
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000', // your frontend's URL (adjust port if different)
-  credentials: true // only needed if you're sending cookies/auth headers
-}));     
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 app.use("/api/admin", adminRoutes);
 app.use("/api/property", propertyRoutes);
 app.use("/api/project", projectRoutes);
-
 app.use("/api/testimonial", testimonialRoutes);
+
 app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "RentalKing Backend Running 🚀",
-    });
+  res.json({
+    success: true,
+    message: "RentalKing Backend Running 🚀",
+  });
 });
 
 export default app;
