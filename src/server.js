cd0772e71180c1b5e31fb755b8f41b575e3f69f0
Sarthak Kasat <kasat4309@gmail.com>
2026-07-27 16:02:00 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,24 +1,19 @@
-import dns from "dns";
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
-
-import dotenv from "dotenv";
-dotenv.config();
-
 import app from "./app.js";
 import connectDB from "./config/db.js";
+import dns from "dns";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
-    try {
-        await connectDB();
-
-        app.listen(PORT, () => {
-            console.log(`🚀 Server running at http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 startServer();
