ff0859f3c2b496765e53b773c516dd7eb6077dab
Sarthak Kasat <kasat4309@gmail.com>
2026-07-27 13:08:37 +0530

    Implement password hashing for admin login

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,5 +1,5 @@
 import Admin from "../models/Admin.js";
-
+import bcrypt from "bcrypt";
 export const loginAdmin = async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -20,12 +20,17 @@ export const loginAdmin = async (req, res) => {
             });
         }
 
-        if (admin.password !== password) {
-            return res.status(401).json({
-                success: false,
-                message: "Invalid email or password."
-            });
-        }
+       const isMatch = await bcrypt.compare(password, admin.password);
+
+if (!isMatch) {
+    return res.status(401).json({
+        success: false,
+        message: "Invalid credentials"
+    });
+}
+req.session.adminId = admin._id;
+req.session.email = admin.email;
+
 
         res.status(200).json({
             success: true,
@@ -42,6 +47,67 @@ export const loginAdmin = async (req, res) => {
         res.status(500).json({
             success: false,
             message: "Internal Server Error."
+        });
+    }
+};
+
+export const logoutAdmin = (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).json({
+                success: false,
+                message: "Logout failed",
+            });
+        }
+
+        res.clearCookie("connect.sid");
+
+        res.json({
+            success: true,
+            message: "Logged out successfully",
+        });
+    });
+};
+
+export const registerAdmin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email and password are required.",
+            });
+        }
+
+        const existingAdmin = await Admin.findOne({ email });
+
+        if (existingAdmin) {
+            return res.status(400).json({
+                success: false,
+                message: "Admin already exists.",
+            });
+        }
+
+        const admin = await Admin.create({
+            email,
+            password, 
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "Admin created successfully.",
+            admin: {
+                id: admin._id,
+                email: admin.email,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
         });
     }
 };
