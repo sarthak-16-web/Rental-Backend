0c40228e87fdca3770f5774e54b2a42af9e582f1
Sarthak Kasat <kasat4309@gmail.com>
2026-07-27 13:08:02 +0530

    Update Admin.js

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const adminSchema = new mongoose.Schema({
     email: {
@@ -14,5 +15,16 @@ const adminSchema = new mongoose.Schema({
         trim: true,
     },
 });
+
+adminSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+adminSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
 
 export default mongoose.model("Admin", adminSchema);
