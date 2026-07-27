e09c62037e69c750a5121c4884b3fae636c13982
Sarthak Kasat <kasat4309@gmail.com>
2026-07-27 13:07:45 +0530

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { loginAdmin } from "../controllers/adminController.js";
+import { loginAdmin  , logoutAdmin , registerAdmin,} from "../controllers/adminController.js";
 
 
 const router = express.Router();
-
+router.post("/register", registerAdmin);
 router.post("/login", loginAdmin);
+router.post("/logout", logoutAdmin);
 
 export default router;
