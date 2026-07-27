c91f0cfa8d6c30c430b58971342f8ac377d8d546
Sarthak Kasat <kasat4309@gmail.com>
2026-07-27 13:06:52 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
-import dns from 'dns';
-dns.setServers(['8.8.8.8', '1.1.1.1']);
+import dns from "dns";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,10 +9,16 @@ import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-console.log("URI:", process.env.MONGODB_URI);
+const startServer = async () => {
+    try {
+        await connectDB();
 
-connectDB();
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running at http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+startServer();
